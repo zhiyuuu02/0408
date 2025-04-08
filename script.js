1e2aab7de041a12feb03a8dafdba373a3bc9825f
zhiyuuu02 <412730995@o365.tku.edu.tw>
2025-04-08 23:01:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener('mousemove', (event) => {
+    const menu = document.querySelector('.menu');
+    if (event.clientY < 250) {
+        menu.style.top = '10px'; // 滑出顯示選單
+    } else {
+        menu.style.top = '-100px'; // 滑入隱藏選單
+    }
+});
+
+function loadIframe(url) {
+    const iframe = document.querySelector('iframe');
+    iframe.src = url; // 設定iframe的來源網址
+    iframe.style.display = 'block'; // 顯示iframe
+}
+
+setInterval(keepRunning, 200);
+
+const img = document.getElementById("img");
+let i=1;
+function keepRunning() {
+    i = (i % 8) + 1;
+    img.src = `IMG/penguin_walk_0${i}.png`;
+  }
